8829102fc3472b5a315f580366c412da7034ebd5
Joao Paulo Silva Piauhy <joaopasip@gmail.com>
2022-07-12 10:27:06 -0300

    documentação da API com swagger

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
 const { router } = require('./routes/index_routes');
+const swaggerFile = require('./swagger_output.json');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(router);
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
